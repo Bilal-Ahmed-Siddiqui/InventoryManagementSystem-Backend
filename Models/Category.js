42480d56c14e768049590925a77973249783a557
Bilal Ahmed <bilalkietacc@gmail.com>
2024-10-23 20:47:54 +0500

    getall and getone endpoint added

--- Models/Category.js
+++ Models/Category.js
@@ -9,7 +9,7 @@ const CategorySchema = mongoose.Schema(
     },
   },
 
-  { timeStamp: true }
+  { timestamps: true }
 );
 
 const Category = mongoose.model("Category", CategorySchema);
--- Routes/CategoryRoutes.js
+++ Routes/CategoryRoutes.js
@@ -16,4 +16,37 @@ router.post("/create", async (req, res)=>{
     }
 })
 
+//get all categories
+router.get("/getAll", async (req, res)=>{
+    try {
+        const categories = await Category.find();
+        if(!categories){
+            return  res.status(404).json({error: "categories not found"})
+         }
+
+        res.status(200).json(categories)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+//get category by id categories
+router.get("/:id", async (req, res)=>{
+    try {
+
+        const {id} = req.params;
+
+        const category = await Category.findOne({_id: id});
+
+        if(!category){
+           return  res.status(404).json({error: "category not found"})
+        }
+
+        res.status(200).json(category)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+
 module.exports = router
